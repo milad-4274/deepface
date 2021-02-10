85ec3b06bb876fa9662f33bb2b22c0a49d67f1c5
milad yousefi <milad.4274@gmail.com>
2021-02-11 00:29:22 +0330

    modification/weight_path

--- test_data.py
+++ test_data.py
@@ -19,14 +19,16 @@ print(get_file_name(galleries, 3))
 print(get_file_name(probes, 3))
 print(get_file_name(probes, 3))
 
+# should be general
+here = os.path.dirname(os.path.realpath(__file__))
+path = here + "/second/images"
 
-path = "/home/milad4274/Desktop/deepface/deepface/second/images"
-
-gallery = get_file_name(galleries, 1)[0]
-for img in get_file_name(probes, 1):
+for i in range(1,121):
+gallery = get_file_name(galleries, i)[0]
+for img in get_file_name(probes, i):
     gpath = osp.join(path, gallery)
     ppath = osp.join(path, img)
-    print(gpath,ppath,"pgpath")
+    # print(gpath,ppath,"pgpath")
     res = DeepFace.verify(gpath, ppath)
     print(img, res)
 
